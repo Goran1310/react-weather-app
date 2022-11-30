cf905a3f35bf70d8eec54cd952b9229b86151695
Goran Lovincic <66091550+Goran1310@users.noreply.github.com>
2022-11-30 14:18:05 +0100

    test

--- src/api.js
+++ src/api.js
@@ -1,11 +1,11 @@
 export const geoApiOptions = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": "",// enter your rapid api key here
+    "X-RapidAPI-Key": "01407c9e54msha66696c13cd93cfp1d3331jsna16042715f87",// enter your rapid api key here
     "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
   },
 };
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
-export const WEATHER_API_KEY = ""; // enter your key from openweather API
+export const WEATHER_API_KEY = "ffa9fc3f5ed1aec1d0f2a2e7ef69b91e"; // enter your key from openweather API
